f956a99136d2954ddb324669f18ec74f733dfeff
aoyama <syouta.aoyama20@gmail.com>
2022-02-17 16:14:38 +0900

    プログラム異常終了の通知のDiscordチャンネルをcontent.jsonから取得するように変更した

--- reverse/error_notify.py
+++ reverse/error_notify.py
@@ -1,9 +1,11 @@
-import requests
 from discordwebhook import Discord
 import json
+import datetime
 
 pathContent = "content.json"
 with open(pathContent) as f:
   content = json.load(f)
-discord = Discord(url=content["discordUrl"])
-discord.post(content="Program is abnormal termination!")
+discord = Discord(url=content["discordErrNotifyURL"])
+
+msg = str(datetime.datetime.now()) + ": Program is abnormal termination!"
+discord.post(content=msg)
